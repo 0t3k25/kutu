7606ef57fa3f599159d1ee32e1b7ec3e7670028f
hureta <40241920+0t3k25@users.noreply.github.com>
2020-11-07 21:12:12 +0900

    modification server.js and pacakge.json

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5432;
-app.listen(port);
-console.log(port);
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('src', path.join(__dirname, 'src'))
+  .set('src engine', 'vue')
+  .get('/', (req, res) => res.render('App'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
