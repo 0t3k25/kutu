6398a1d5f42016bba82281532866329adac32265
hureta <40241920+0t3k25@users.noreply.github.com>
2021-02-02 16:02:47 +0900

    once finished

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,25 +2,17 @@
   <div class="fixed-top">
     <b-navbar toggleable="lg" type="dark" variant="dark">
       <b-navbar-brand href="#">
-        <img class="MainLogo" src="#" />
-        <b-navbar-brand href="#" class="pl-2">KUTU</b-navbar-brand>
+        <b-navbar-brand href="#" class="pl-3">KUTU</b-navbar-brand>
       </b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="mx-auto">
-          <b-nav-text class="”">歩きやすい靴・オシャレな靴</b-nav-text>
-          <!-- 小さくした際歩きやすい靴・おしゃれな靴の表示がおかしい-->
-        </b-navbar-nav>
-
         <!-- Right aligned nav items -->
-        <b-navbar-nav right>
+        <b-navbar-nav class="ml-auto">
           <b-nav-item>マイページ</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
-    <nav class="period"></nav>
   </div>
 </template>
 
@@ -28,60 +20,10 @@
 export default {
   name: "Header",
   props: {
-    msg: String
-  }
+    msg: String,
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.MainLogo {
-  height: 40px;
-}
-@-webkit-keyframes period-color {
-  0% {
-    background-color: red;
-  }
-  10% {
-    background-color: yellow;
-  }
-  20% {
-    background-color: greenyellow;
-  }
-  30% {
-    background-color: skyblue;
-  }
-  40% {
-    background-color: blueviolet;
-  }
-  50% {
-    background-color: red;
-  }
-}
-
-@keyframes period-color {
-  0% {
-    background-color: red;
-  }
-  20% {
-    background-color: yellow;
-  }
-  40% {
-    background-color: greenyellow;
-  }
-  60% {
-    background-color: skyblue;
-  }
-  80% {
-    background-color: blueviolet;
-  }
-  100% {
-    background-color: red;
-  }
-}
-.period {
-  padding: 1.5px;
-  animation: period-color 15s infinite;
-  -webkit-animation: period-color 15s infinite;
-}
-</style>
+<style scoped></style>
