e94a7ba347ca83001f7cdee8ca57b982c4b82895
hureta <40241920+0t3k25@users.noreply.github.com>
2021-02-08 22:01:27 +0900

    change product_men

---
+++ src/views/Product_men.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="product_men">
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "men",
+  data() {
+    return {
+      name: "Product_mem",
+    };
+  },
+};
+</script>
