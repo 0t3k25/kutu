b61f66204f1a0f2cf6e52b7e31161033be5761cc
hureta <40241920+0t3k25@users.noreply.github.com>
2021-02-16 23:44:19 +0900

    standard item component

---
+++ src/components/Standard_item.vue
@@ -0,0 +1,24 @@
+<!-- 定番商品のコンポーネントSrandard.vueに使用 -->
+<template>
+  <div id="standard_item" class="container">
+    <div>
+      <h1 class="text text-center">{{ name }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Standard_item",
+  data() {
+    return {
+      name: "現在準備中",
+    };
+  },
+};
+</script>
+<style scoped>
+.text {
+  padding: 100px 200px 200px 200px;
+}
+</style>
